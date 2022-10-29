72734e1d8db51256dd085b714ab9d1aa4b661fd9
Arn27 <116111992+Arn27@users.noreply.github.com>
2022-10-29 21:48:25 +0100

    Add files via upload

---
+++ bibliotekos.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <algorithm> 
+
+using std::cin;
+using std::cout;
+using std::istream;
+using std::ostream;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+using std::string;
+using std::vector;
+
+#include "funkcijos.h"
+#include "funkcijos.cpp"
